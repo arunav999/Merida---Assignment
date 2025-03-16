aa52b9a8f894aa47e7d24b3083f5378cbae5e647
Arunav Singh <arunavsingh999@gmail.com>
2025-03-16 19:27:26 +0530

    completed the design of the table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,16 +5,64 @@ function Table() {
     <table className="table">
       <thead className="table__head">
         <tr>
-          <td>SI No</td>
-          <td>Task Name</td>
-          <td>Created Date</td>
-          <td>Due Date</td>
-          <td>Completed Date</td>
-          <td>Status</td>
-          <td>Action</td>
+          <th>SI No</th>
+          <th>Task Name</th>
+          <th>Created Date</th>
+          <th>Due Date</th>
+          <th>Completed Date</th>
+          <th>Status</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody className="table__body">
+        <tr>
+          <td>1</td>
+          <td>ABZ</td>
+          <td>today's date</td>
+          <td>get date from the user</td>
+          <td>
+            whenever user clicks on completed in action on that date or after
+            due date get that date
+          </td>
+          <td>completed / completed after due date</td>
+          <td>button - completed ?</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>ABZ</td>
+          <td>today's date</td>
+          <td>get date from the user</td>
+          <td>
+            whenever user clicks on completed in action on that date or after
+            due date get that date
+          </td>
+          <td>completed / completed after due date</td>
+          <td>button - completed ?</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>ABZ</td>
+          <td>today's date</td>
+          <td>get date from the user</td>
+          <td>
+            whenever user clicks on completed in action on that date or after
+            due date get that date
+          </td>
+          <td>completed / completed after due date</td>
+          <td>button - completed ?</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>ABZ</td>
+          <td>today's date</td>
+          <td>get date from the user</td>
+          <td>
+            whenever user clicks on completed in action on that date or after
+            due date get that date
+          </td>
+          <td>completed / completed after due date</td>
+          <td>button - completed ?</td>
+        </tr>
         <tr>
           <td>1</td>
           <td>ABZ</td>
